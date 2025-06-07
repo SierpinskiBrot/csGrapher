da762452e70a6d2138d753b68880fea9d9460a31
SierpinskiBrot <gavinolsy@outlook.com>
2025-06-07 05:45:22 -0600

    big css changes

--- scripts.js
+++ scripts.js
@@ -6,6 +6,73 @@ var logScale = [false, false]
 window.selectedSess = 0; //selected session from the cstimer
 
 
+window.currentTheme = 'gold'
+const themes = {
+  green: {
+    '--color-primary': '#128629',
+    '--color-primary-variant': '#2ea93f',
+    '--color-secondary': '#84c750',
+    '--color-secondary-variant': '#9bd171',
+    '--color-background': '#5ac465',
+    '--color-surface:': '#d4ebc2',
+    '--color-surface-odd': '#eef7e6',
+    '--color-error': '#128629',
+
+    '--on-primary': '#e7f6e9',
+    '--on-secondary': '#006400',
+    '--on-background': '#006713',
+    '--on-surface': '#006400',
+    '--on-error': '#F2E8CF',
+  },
+  blue: {
+    '--color-primary': '#6200EE',
+    '--color-primary-variant': '#3700B3',
+    '--color-secondary': '#03DAC6',
+    '--color-secondary-variant': '#018786',
+    '--color-background': '#FFFFFF',
+    '--color-surface': '#D6D6D6',
+    '--color-surface-odd': '#CCCCCC',
+    '--color-error': '#B00020',
+    '--on-primary': '#FFFFFF',
+    '--on-secondary': '#000000',
+    '--on-background': '#000000',
+    '--on-surface': '#000000',
+    '--on-error': '#FFFFFF',
+  },
+  gold: {
+    '--color-primary':' #db3a34',
+    '--color-primary-variant':' #ad201c',
+    '--color-secondary':' #e4a547',
+    '--color-secondary-variant':' #eec98c',
+    '--color-background':' #e3e673',
+    '--color-surface':' #f3f4c2',
+    '--color-surface-odd':' #fafbe7',
+    '--color-error':' #ad201c',
+
+    '--on-primary':' #fafbe7',
+    '--on-secondary':' #ad201c',
+    '--on-background':' #877614',
+    '--on-surface':' #ad201c',
+    '--on-error':' #ebfce9',
+  }
+};
+
+function applyTheme(theme) {
+    window.currentTheme = theme
+    const root = document.documentElement;
+    const colors = themes[theme];
+    for (let key in colors) {
+        root.style.setProperty(key, colors[key]);
+    }
+    window.h.updateOptions({color: themes[theme]['--color-primary']})
+}
+document.getElementById("themeGreen").addEventListener("click", function() {applyTheme("green")})
+document.getElementById("themeBlue").addEventListener("click", function() {applyTheme("blue")})
+document.getElementById("themeGold").addEventListener("click", function() {applyTheme("gold")})
+//applyTheme("icy")
+
+
+
 function binarySearchInsertIdx(arr, val) {
     let low = 0, high = arr.length;
     while (low < high) {
@@ -105,7 +172,8 @@ function createSeriesRow(i) {
     }, "seriesToggle")
     //set the color
     const color1 = window.userData.colors[i - 1];
-    newButton1.style = "background:" + color1 + "; border-color: " + color1
+    //newButton1.style = "background:" + color1 + "; border-color: " + color1
+    newButton1.style = "box-shadow: 2px 2px 3px 3px" + color1
     //redundant as visibility is reset when a file is uploaded
     if (!window.userData.visibilities[i - 1]) newButton1.classList.toggle('pressed')
 
@@ -119,7 +187,8 @@ function createSeriesRow(i) {
     }, "seriesToggle")
     //set the color
     const color2 = window.userData.colors[i];
-    newButton2.style = "background:" + color2 + "; border-color: " + color2
+    //newButton2.style = "background:" + color2 + "; border-color: " + color2
+    newButton2.style = "box-shadow: 2px 2px 3px 3px" + color2
     //redundant as visibility is reset when a file is uploaded
     if (!window.userData.visibilities[i]) newButton2.classList.toggle('pressed')
 
@@ -141,6 +210,7 @@ function createSeriesRow(i) {
         const widthSelector = document.getElementById("seriesWidthSelector")
         widthSelector.value = window.userData.widths[i - 1];
     }, "seriesSettings")
+    //seriesSettings.style = "box-shadow: 2px 2px 3px 3px" + color2
 
     const pbSeriesButton = createButton(window.userData.labels[i+1], (e) => {
         //make all the other buttons untoggled
@@ -153,6 +223,7 @@ function createSeriesRow(i) {
         window.userData.currentPbSeries = window.userData.labels[i+1]
     }, "seriesToggle pbSeriesSelectButton")
     if(window.userData.currentPbSeries == window.userData.labels[i+1]) {pbSeriesButton.classList.toggle('pressed')}
+    //newButton2.style = "background:" + color2 + "; border-color: " + color2
 
     const cell1 = document.createElement("td")
     cell1.appendChild(newButton1)
@@ -291,8 +362,8 @@ const statsButton = document.getElementById("statsButton");
 const graphContainer = document.getElementById("graphContainer");
 const histogramContainer = document.getElementById("histogramContainer");
 const statsContainer = document.getElementById("statsContainer");
-const graphLegend = document.getElementById("graphLegend")
-const histLegend = document.getElementById("histLegend")
+//const graphLegend = document.getElementById("graphLegend")
+//const histLegend = document.getElementById("histLegend")
 const pageBody = document.getElementById("body")
 function resetContainers() {
     histogramContainer.style.display = "none";
@@ -301,8 +372,8 @@ function resetContainers() {
     statsButton.classList.remove("pressed");
     graphContainer.style.display = "none";
     graphButton.classList.remove("pressed");
-    graphLegend.style.display = "none"
-    histLegend.style.display = "none"
+    //graphLegend.style.display = "none"
+    //histLegend.style.display = "none"
     pageBody.style.overflow = "hidden"
 }
 graphButton.addEventListener("click", function() {
@@ -312,7 +383,7 @@ graphButton.addEventListener("click", function() {
     graphButton.classList.add("pressed");
     window.g.resize();
     window.updateGraph(); window.g.resetZoom()
-    graphLegend.style.display = "block"
+    //graphLegend.style.display = "block"
 })
 histogramButton.addEventListener("click", function() {
     window.currentTab = "hist";
@@ -325,7 +396,7 @@ histogramButton.addEventListener("click", function() {
         window.userData.genSlidingWindowDefaults()
         window.userData.genCreationDefaults();
     }
-    histLegend.style.display = "block"
+    //histLegend.style.display = "block"
 })
 statsButton.addEventListener("click", function() {
     window.currentTab = "stats";
@@ -419,8 +490,8 @@ colorSelector.addEventListener("change", function () {
     //update the color of the series toggle buttons
     const toggleButtons = document.getElementsByClassName("seriesToggle")
     for (let i = 1; i <= 2; i++) {
-        toggleButtons[seriesNumber - i].style.background = window.userData.colors[seriesNumber - i]
-        toggleButtons[seriesNumber - i].style.borderColor = window.userData.colors[seriesNumber - i]
+        toggleButtons[seriesNumber - i].style = "box-shadow: 2px 2px 3px 3px " + window.userData.colors[seriesNumber - i]
+        toggleButtons[seriesNumber - i].style = "box-shadow: 2px 2px 3px 3px " + window.userData.colors[seriesNumber - i]
     }
 })
 //the width selector
@@ -510,10 +581,12 @@ jsonDataFile.addEventListener("change", function() {
                     labels: window.userData.labels,
                     xlabel: window.userData.xTitle,
                     ylabel: "Time(s)",
-                    legend: "always",
+                    //legend: "always",
+                    legend: "follow",
                     color: "#084C61",
-                    labelsDiv: document.getElementById("graphLegend"),
-                    labelsSeparateLines: false,
+                    //color: themes[window.currentTheme]['--color-primary']
+                    //labelsDiv: document.getElementById("graphLegend"),
+                    //labelsSeparateLines: false,
                 }
             );
             //Create the histogram
@@ -526,10 +599,12 @@ jsonDataFile.addEventListener("change", function() {
                     ylabel: "Frequency",
                     stepPlot: true,
                     fillGraph: true,
-                    color: "#DB3A34",
+                    //color: "#128629",
+                    color: themes[window.currentTheme]['--color-primary'],
+                    legend: "follow",
                     fillAlpha: 0.5,
-                    labelsDiv: document.getElementById("histLegend"),
-                    labelsSeparateLines: false,
+                    //labelsDiv: document.getElementById("histLegend"),
+                    //labelsSeparateLines: false,
                 }
             );
         });
@@ -1161,14 +1236,11 @@ class UserData {
         
     }
 
-
+    //create the list for stats tab
     updatePBTable(sess,series) {
-        //console.log("Updating pb table to session " + j)
-        //create the list for stats tab
+
         const pbStatsBody = document.getElementById("pbStatsBody")
-        //const headerRow = document.getElementById("pbStatsHeader")
         pbStatsBody.replaceChildren();
-        //pbStats.appendChild(headerRow);
 
         for(let i = this.pbData[sess][series][1].length-1; i >= 0; i--) {
             let newRow = document.createElement("tr");
@@ -1176,18 +1248,18 @@ class UserData {
             //Date column
             let dateCol = document.createElement("td");
             let date = this.pbData[sess][series][2][i];
-            let dateStr = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+            let dateStr = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear(); //month is 0-indexed for some reason
             dateCol.innerHTML = dateStr;
 
             //PB For Time column
             let date2 = new Date();
             let dateDiff = 0;
             let pbForTimeCol = document.createElement("td")
-            if(i == this.pbData[sess][series][1].length-1) {
+            if(i == this.pbData[sess][series][1].length-1) { //most recent record
                 dateDiff = Math.abs(date2-date);
                 pbForTimeCol.innerHTML = dhm(dateDiff) + " and counting";
             }
-            else {
+            else { //everything else
                 date2 = this.pbData[sess][series][2][i+1];
                 dateDiff = Math.abs(date2-date)
                 pbForTimeCol.innerHTML = dhm(dateDiff);
